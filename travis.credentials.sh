f49479981febbf7df2b1ba99c11e4b9ca63af6dd
Lukasz Jastrzebski <lukasz.jastrzebski@nokia.com>
2016-10-19 08:55:18 -0700

    Trying to fix travis build

--- travis.credentials.sh
+++ travis.credentials.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
+echo "Creating .bintray/.credentials"
 mkdir ~/.bintray/
 BINTRAY_FILE=$HOME/.bintray/.credentials
-ARTIFACTORY_FILE=$HOME/.bintray/.artifactory
 cat <<EOF >$BINTRAY_FILE
 realm = Bintray API Realm
 host = api.bintray.com
 user = $BINTRAY_USER
 password = $BINTRAY_PASSWORD
 EOF
- 
-cat <<EOF >$ARTIFACTORY_FILE
-realm = Artifactory Realm
-host = oss.jfrog.org
-user = $BINTRAY_USER
-password = $BINTRAY_PASSWORD
-EOF
+
+head -n 3 $BINTRAY_FILE
